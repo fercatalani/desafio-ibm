708eb3f9553564fe2cda9b8f16f3564de9866b5e
Fernanda Caleffi Catalani <63620168+fercatalani@users.noreply.github.com>
2021-07-28 16:01:47 -0300

    pasta de componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import api from './api';
+//import api from './api';
 
 class App extends Component{
 
   render(){
     return(
       <div>
-        
+        <h1>Novo projeto</h1>
       </div>
     );
   };
