eead2ca0705705e4d446c35530efff3073e45878
Maria Eduarda Santo <maria.eduarda.santo@ibm.com>
2021-07-29 12:58:53 -0300

    feat: adiciona lista de livros

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 import Search from "./components/searchComponent";
+import { BookDetail } from "./components/BookDetail";
 
 import "./App.css";
 //import api from './api';
+
+const books = [
+  {
+    title: "Ensaio sobre a Cegueira",
+    description:
+      "Ensaio sobre a Cegueira é um romance do escritor português José Saramago, publicado em 1995, e traduzido para diversas línguas. A obra narra a história da epidemia de cegueira branca que se espalha por uma cidade, causando um grande colapso na vida das pessoas e abalando as estruturas sociais.",
+    author: "José Saramago",
+    price: "R$ 50,00",
+    favorite: true,
+  },
+  {
+    title: "Dom Casmurro",
+    description:
+      "Prometido para o seminário desde o nascimento, o jovem carioca Bentinho precisa encontrar um jeito de fugir da vida na Igreja e realizar seu verdadeiro sonho: casar-se com a vizinha Capitu. Uma história de paixão, obsessão e ciúme se desenrola, em uma narrativa cheia de reviravoltas, que aos poucos constrói um retrato da sociedade brasileira.",
+    author: "Machado de Assis",
+    price: "R$ 29,90",
+    favorite: false,
+  },
+  {
+    title: "Dom Quixote",
+    description:
+      "Dom Quixote de la Mancha é um livro escrito pelo espanhol Miguel de Cervantes. O título e ortografia originais eram El ingenioso hidalgo Don Quixote de La Mancha, com sua primeira edição publicada em Madrid no ano de 1605.",
+    author: "Miguel de Cervantes",
+    price: "R$ 45,00",
+    favorite: true,
+  },
+];
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Search />
+
+        <section className="list-container">
+          {books.map((book) => (
+            <BookDetail book={book} />
+          ))}
+        </section>
       </div>
     );
   }
