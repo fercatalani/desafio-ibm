618643f8b9467b4194908eefa15efabf1f3379f2
Fernanda Caleffi Catalani <63620168+fercatalani@users.noreply.github.com>
2021-07-30 17:45:45 -0300

    desenvolvimento da nav

--- src/App.js
+++ src/App.js
@@ -1,64 +1,33 @@
-import React, { useState } from "react";
-import Search from "./components/Search";
-import { BookDetail } from "./components/BookDetail";
-import Dropdown from "./components/Dropdown";
-
-import categories from "./data/categories.json"
-
+import React from "react";
 import "./App.css";
-//import api from './api'
 
-const books = [
-  {
-    title: "Ensaio sobre a Cegueira",
-    description:
-      "Ensaio sobre a Cegueira é um romance do escritor português José Saramago, publicado em 1995, e traduzido para diversas línguas. A obra narra a história da epidemia de cegueira branca que se espalha por uma cidade, causando um grande colapso na vida das pessoas e abalando as estruturas sociais.",
-    author: "José Saramago",
-    price: "R$ 50,00",
-    favorite: true,
-  },
-  {
-    title: "Dom Casmurro",
-    description:
-      "Prometido para o seminário desde o nascimento, o jovem carioca Bentinho precisa encontrar um jeito de fugir da vida na Igreja e realizar seu verdadeiro sonho: casar-se com a vizinha Capitu. Uma história de paixão, obsessão e ciúme se desenrola, em uma narrativa cheia de reviravoltas, que aos poucos constrói um retrato da sociedade brasileira.",
-    author: "Machado de Assis",
-    price: "R$ 29,90",
-    favorite: false,
-  },
-  {
-    title: "Dom Quixote",
-    description:
-      "Dom Quixote de la Mancha é um livro escrito pelo espanhol Miguel de Cervantes. O título e ortografia originais eram El ingenioso hidalgo Don Quixote de La Mancha, com sua primeira edição publicada em Madrid no ano de 1605.",
-    author: "Miguel de Cervantes",
-    price: "R$ 45,00",
-    favorite: true,
-  },
-];
+import { Header } from "./components/Header";
+import Nav from "./Nav";
+import Home from "./Home";
+import NewBook from "./NewBook";
+
+//renomeando para Router
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+/* BrowserRouter: tudo que estiver dentro dele tera a habilidade de roteamento
+Route: renderiza um componente com base na URL com path e especificamos qual componente em seguida
+Switch: assim que ele identifica um path, ele renderiza o componente
+
+exact: evita renderizar tudo que for identificado, e renderiza apenas aquele path exato
+*/
 
 export default function App() {
-  const [value, setValue] = useState(null)
-  
-    return (
+  return (
+    <Router>
       <div className="App">
-
-        <div className="containerFilter">
-          <Search />
-          <div style={{ width: 200}}>
-            {/* options: referenciando os dados, prompt: especificando a mensagem */}
-            <Dropdown 
-              options={categories} 
-              prompt='Categoria' 
-              value={value}
-              onChange={val => setValue(val)}/>
-          </div>
-        </div>
-
-        <section className="list-container">
-          {books.map((book) => (
-            <BookDetail book={book} />
-          ))}
-        </section>
-
+        <Header>
+        <Nav />
+        </Header>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/new-book" component={NewBook} />
+        </Switch>
       </div>
-    );
+    </Router>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Header } from "./components/Header";
 import App from "./App";
-import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
