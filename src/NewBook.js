14f804799112cd3d70bc633c4a8202552e7756d7
Fernanda Caleffi Catalani <63620168+fercatalani@users.noreply.github.com>
2021-07-30 17:51:44 -0300

    criacao dos arq pra nav

---
+++ src/NewBook.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './NewBook.css';
+
+import Form from "./components/Form";
+
+export default function NewBook() {
+    return (
+      <div className="newbook-container">
+        <Form />
+      </div>
+    );
+};
