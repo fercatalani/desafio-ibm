f6e4d4d12e4b0bee3f2de6d0c96186204fd41a9e
Alex Silva <alexifb1985@gmail.com>
2021-08-03 19:14:11 -0300

    feat: busca por nome

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
 
-import Search from "../../components/Search";
 // import { BookDetail } from "../Livro/BookDetail";
 import Dropdown from "../../components/Dropdown";
 import api from "../../services/api";
+import searchIcon from "../../assets/searchIcon.svg";
 
 import categories from "../../mock/categories.json";
 import { AiOutlineArrowRight } from "react-icons/ai";
@@ -13,18 +13,44 @@ export default function Home() {
   const [value, setValue] = useState(null);
 
   const [livros, setLivros] = useState([]);
+  const [repos, setRepos] = useState([])
 
   useEffect(() => {
-    api.get("/livros").then(({ data }) => {
+    api.get("/books").then(({ data }) => {
       setLivros(data);
+      setRepos(data);
       console.log(JSON.stringify(data));
     });
   }, []);
 
+
+  const handleChange = ({ target }) => {
+    if (!target.value) {
+      setLivros(repos);
+      return
+    }
+    const filterLivros = repos.filter(({ nome }) => nome.includes(target.value));
+
+    setLivros(filterLivros);
+  }
+
+
   return (
     <div className="home-container">
       <div className="containerFilter">
-        <Search />
+        <div className="search">
+          <input
+            className="input"
+            type="text"
+            placeholder="Busca..."
+            onChange={handleChange}
+          />
+          <img
+            src={searchIcon}
+            alt="search icon"
+            className="icon"
+          />
+        </div>
         <div style={{ width: 200 }}>
           {/* options: referenciando os dados, prompt: especificando a mensagem */}
           <Dropdown
