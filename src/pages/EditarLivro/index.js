ab2646b1984104a9a389a6c76722b6ac5289cb58
Maria Eduarda Santo <maria.eduarda.santo@ibm.com>
2021-08-03 13:36:52 -0300

    feat: adiciona funções de post e put de livro

--- src/pages/EditarLivro/index.js
+++ src/pages/EditarLivro/index.js
@@ -1,12 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { useHistory, useParams } from "react-router-dom";
+import api from "../../services/api";
 import "./styles.css";
 import bookImg from "../../assets/images/book.svg";
-import api from "../../services/api";
-import { useHistory, useParams } from "react-router-dom";
 
-export default function EditBook(props) {
+export default function EditBook() {
   const [categories, setCategories] = useState([]);
-  const [book, setBook] = useState({});
+  const [book, setBook] = useState({
+    categoria_id: "",
+    nome: "",
+    descricao: "",
+    estoque: 0,
+    classificacao: 0,
+    sbn: "",
+  });
   const params = useParams();
   const history = useHistory();
 
@@ -26,7 +33,12 @@ export default function EditBook(props) {
     }
   };
 
-  const handleSubmit = () => {};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    api.put(`/livro/${params.id}`, book).then(() => {
+      history.goBack();
+    });
+  };
 
   return (
     <div className="newbook-container">
@@ -41,33 +53,57 @@ export default function EditBook(props) {
             <h2>Edite seu livro</h2>
             <div>
               <label>Nome</label>
-              <input value={book.nome} />
+              <input
+                value={book.nome}
+                onChange={(e) => setBook({ ...book, nome: e.target.value })}
+              />
               <br />
             </div>
             <div>
               <label>Descrição</label>
-              <input value={book.descricao} />
+              <input
+                value={book.descricao}
+                onChange={(e) =>
+                  setBook({ ...book, descricao: e.target.value })
+                }
+              />
               <br />
             </div>
 
             <div>
               <label>Estoque</label>
-              <input value={book.estoque} />
+              <input
+                value={book.estoque}
+                onChange={(e) => setBook({ ...book, estoque: e.target.value })}
+              />
               <br />
             </div>
             <div>
               <label>Classificação</label>
-              <input value={book.classificacao} />
+              <input
+                value={book.classificacao}
+                onChange={(e) =>
+                  setBook({ ...book, classificacao: e.target.value })
+                }
+              />
               <br />
             </div>
             <div>
               <label>SBN</label>
-              <input value={book.sbn} />
+              <input
+                value={book.sbn}
+                onChange={(e) => setBook({ ...book, sbn: e.target.value })}
+              />
               <br />
             </div>
             <div>
               <label>Categoria</label>
-              <select>
+              <select
+                value={book.category?.id}
+                onChange={(e) =>
+                  setBook({ ...book, categoria_id: e.target.value })
+                }
+              >
                 {categories.map((category) => (
                   <option key={category.id} value={category.id}>
                     {category.name}
--- src/pages/Livro/index.jsx
+++ src/pages/Livro/index.jsx
@@ -1,41 +1,116 @@
-import React from "react";
-import { div } from 'prelude-ls';
-import './styles.css';
-import book from '../../assets/images/book.svg'; 
-
-
+import React, { useEffect, useState } from "react";
+import api from "../../services/api";
+import "./styles.css";
+import bookImg from "../../assets/images/book.svg";
 
 export default function NewBook() {
-    return (
-      <div className="newbook-container">
-             <div className="screen">
-            <section className="image">
-                <img src={book} alt={"logo"} />
-                <h2>Cadastre seus livros</h2>
-            </section>
+  const [categories, setCategories] = useState([]);
+  const [book, setBook] = useState({
+    categoria_id: "",
+    nome: "",
+    descricao: "",
+    estoque: 0,
+    classificacao: 0,
+    sbn: "",
+  });
 
-            <section className='FormS'>
-                <form>
-                    <h2>API Livro-Teste</h2>
-                    <div>
-                        <label>Livro</label>
-                        <input /><br />
-                    </div>
-                    <div><label>Autor</label>
-                        <input /><br /></div>
+  useEffect(() => {
+    api.get("/categories").then((response) => setCategories(response.data));
+  }, []);
 
-                    <div>
-                        <label>Valor</label>
-                        <input /><br />
-                    </div>
-                    <div>
-                        <label>Descrição:</label>
-                        <input /><br />
-                    </div>
-                    <button>Cadastrar</button>
-                </form>
-            </section>
-        </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    api.post("/livro", book).then(() => {
+      window.alert("Seu livro foi cadastrado com sucesso!");
+      setBook({
+        categoria_id: "",
+        nome: "",
+        descricao: "",
+        estoque: 0,
+        classificacao: 0,
+        sbn: "",
+      });
+    });
+  };
+
+  return (
+    <div className="newbook-container">
+      <div className="screen">
+        <section className="image">
+          <img src={bookImg} alt={"logo"} />
+          <h2>Cadastre seus livros</h2>
+        </section>
+        <section className="FormS">
+          <form onSubmit={handleSubmit}>
+            <h2>Cadastre seu livro</h2>
+            <div>
+              <label>Nome</label>
+              <input
+                value={book.nome}
+                onChange={(e) => setBook({ ...book, nome: e.target.value })}
+              />
+              <br />
+            </div>
+            <div>
+              <label>Descrição</label>
+              <input
+                value={book.descricao}
+                onChange={(e) =>
+                  setBook({ ...book, descricao: e.target.value })
+                }
+              />
+              <br />
+            </div>
+
+            <div>
+              <label>Estoque</label>
+              <input
+                type="number"
+                value={book.estoque}
+                onChange={(e) => setBook({ ...book, estoque: e.target.value })}
+              />
+              <br />
+            </div>
+            <div>
+              <label>Classificação</label>
+              <input
+                type="number"
+                value={book.classificacao}
+                onChange={(e) =>
+                  setBook({ ...book, classificacao: e.target.value })
+                }
+              />
+              <br />
+            </div>
+            <div>
+              <label>SBN</label>
+              <input
+                value={book.sbn}
+                onChange={(e) => setBook({ ...book, sbn: e.target.value })}
+              />
+              <br />
+            </div>
+            <div>
+              <label>Categoria</label>
+              <select
+                value={book.categoria_id}
+                onChange={(e) =>
+                  setBook({ ...book, categoria_id: e.target.value })
+                }
+              >
+                {categories.map((category) => (
+                  <option key={category.id} value={category.id}>
+                    {category.name}
+                  </option>
+                ))}
+              </select>
+              <br />
+            </div>
+            <button type="submit">Cadastrar</button>
+          </form>
+        </section>
       </div>
-    );
-};
+    </div>
+  );
+}
