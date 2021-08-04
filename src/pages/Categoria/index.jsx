5f63becd1a0881d6d3ea2446bafb9be0c0da148e
Fernanda Caleffi Catalani <63620168+fercatalani@users.noreply.github.com>
2021-08-04 12:57:55 -0300

    feat: cria metodo delete

--- src/pages/Categoria/index.jsx
+++ src/pages/Categoria/index.jsx
@@ -2,20 +2,26 @@ import React, { useEffect, useState } from 'react';
 import book from '../../assets/images/book.svg';
 import './styles.css';
 import api from "../../services/api";
-
+import { FaTimesCircle } from "react-icons/fa";
 
 export default function NovaCategoria() {
   const [categorias, setCategorias] = useState(null);
   const [category, setCategory] = useState("")
 
+  //GET
   useEffect(() => {
+    listCategory();
+  }, []);
+  
+  function listCategory() {
     api.get("/categories")
       .then((response) => {
         setCategorias(response.data);
         console.log(JSON.stringify(response.data));
       });
-  }, []);
+  }
 
+  //POST
   function createCategory() {
     api
       .post("/category", category)
@@ -23,6 +29,16 @@ export default function NovaCategoria() {
         setCategory(response.data);
         console.log(JSON.stringify(response.data));
       });
+  }
+
+  //DELETE
+  function deleteCategory(id) {
+    api
+      .delete(`/category/${id}`)
+      .then((resp) => {
+        console.warn(resp);
+        listCategory();
+      })
   }
 
   return(
@@ -39,6 +55,7 @@ export default function NovaCategoria() {
             <div>
               <label>Categoria</label>
               <input 
+              type="text"
               value={category.name}
               onChange={(e) => setCategory({
                 ...categorias, name: e.target.value
@@ -48,11 +65,19 @@ export default function NovaCategoria() {
             <button onClick={createCategory}>Cadastrar</button>
             
           <h2>Lista de categorias</h2>
-            {categorias?.map(categoria => (
+            {categorias?.map(categoria => {
+              return (
               <ul className="listagem">
                 <li key={categoria.id}>{categoria.name}</li>
+                <div className="delButton">
+                  <FaTimesCircle 
+                    onClick={() => deleteCategory(categoria.id)}
+                    fontSize="35px" 
+                    color="#D2042D"
+                    />
+                </div>
               </ul>
-            ))}
+            )})}
 
           </form>
         </section>
