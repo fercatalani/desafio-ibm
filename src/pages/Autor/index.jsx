1e7743aff052abd892b59953712c8d471bb8c32f
Rodrigo Dias Maximiano <rodrigo.dias@pearson.com>
2021-08-04 12:48:08 -0300

    Parametrização inicial do endpoint + form cadastro.

--- src/pages/Autor/index.jsx
+++ src/pages/Autor/index.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./styles.css";
+import { InsertAuthor } from '../../services/api'
 
 function Autor() {
+    const [ author, setAuthor] = useState({
+        name: '',
+        birthdate: '',
+        nationality: '',
+        biography: ''
+    })
+
+    async function SaveAuthor(){
+        const data = author;
+
+        console.log('Dados autor ===> ', author);
+
+        const resp = await InsertAuthor(data)
+
+        console.log('Retorno Endpoint cadastro do autor ==> ', resp)
+    }
+
+
   return (
     <div className="container_autor">
       <div className="topbar_autor">
@@ -12,22 +31,22 @@ function Autor() {
           <div className="titulo">Cadastro de autor</div>
           <div>
             Nome:
-            <input type="text"></input>
+            <input type="text" value={author.name} onChange={(e) => setAuthor({...author, name: e.target.value})}></input>
           </div>
           <div>
             Nacionalidade:
-            <input type="text"></input>
+            <input type="text" value={author.nationality} onChange={(e) => setAuthor({...author, nationality: e.target.value})}></input>
           </div>
           <div>
             Aniversario:
-            <input type="text"></input>
+            <input type="text" value={author.birthdate} onChange={(e) => setAuthor({...author, birthdate: e.target.value})}></input>
           </div>
           <div>
             Biografia:
-            <input type="text"></input>
+            <input type="text" value={author.biography} onChange={(e) => setAuthor({...author, biography: e.target.value})} maxLength="255"></input>
           </div>
           <div>
-            <button type="button">Salvar</button>
+            <button type="button" onClick={() => SaveAuthor()}>Salvar</button>
           </div>
         </div>
       </div>
--- src/services/api.js
+++ src/services/api.js
@@ -6,3 +6,68 @@ const api = axios.create({
 })
 
 export default api;
+
+
+// AUTHOR API
+// Retorna lista de autor
+export const GetAuthor = () => {
+    const resp = api.get('/author').then((response) => {
+        return response.data
+    }).catch((err) => {
+        console.log(err.response)
+    })
+
+    return resp
+}
+
+// Retorna autor baseado no id.
+export const GetAuhorByID = (id) => {
+    const resp = api.get(`/author/${id}`).then((response) => {
+        return response.data
+    }).catch((err) => {
+        console.log(err.response)
+    })
+
+    return resp
+}
+
+// Retorna autor baseado no nome
+export const GetAuhorByName = (nome) => {
+    const resp = api.get(`/findByNameContaining?name=${nome}`).then((response) => {
+        return response.data
+    }).catch((err) => {
+        console.log(err.response)
+    })
+
+    return resp
+}
+
+// Retorna autor baseado no nome ou bibliografia
+export const GetAuhorByNameAndBiography = (name, biography) => {
+    const resp = api.get(`/findByNameOrBiographyContaining?=biography${biography}&name=${name}`).then((response) => {
+        return response.data
+    }).catch((err) => {
+        console.log(err.response)
+    })
+
+    return resp
+}
+
+// Insert de autor by Id
+export const InsertAuthor = async (data) => {
+    console.log('Dados recebido pelo formulario ==> ', data)
+    const resp = await api.post('/author', data);
+    console.log('Retorno InsertLivro ==> ', resp)
+}
+
+// Delete de autor by Id
+export const DeleteAuthor = async (id) => {
+    const resp = await api.delete(`/author/${id}`)
+    console.log('DeleteLivro ==> ', resp.response)
+}
+
+// Update de autor
+export const UpdateAuthor = async (data) => {
+    const resp = await api.put('/author', data)
+    console.log('UpdateLivro ==> ', resp.response)
+}
