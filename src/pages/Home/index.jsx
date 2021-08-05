787dea268ddae31d3626f07c847ea24d6dba75fe
Alex Silva <alexifb1985@gmail.com>
2021-08-05 15:04:00 -0300

    feat: paginação

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -15,6 +15,16 @@ export default function Home() {
   const [livros, setLivros] = useState([]);
   const [repos, setRepos] = useState([])
 
+  const [itemsPerPage, setItemsPerPage] = useState(5);
+  const [currentPage, setCurrentPage] = useState(0)
+
+
+  const pages = Math.ceil(livros.length / itemsPerPage);
+  const startIndex = currentPage * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentItems = livros.slice(startIndex, endIndex)
+
+
   useEffect(() => {
     api.get("/books").then(({ data }) => {
       setLivros(data);
@@ -22,6 +32,7 @@ export default function Home() {
       console.log(JSON.stringify(data));
     });
   }, []);
+
 
 
   const handleChange = ({ target }) => {
@@ -36,7 +47,7 @@ export default function Home() {
 
 
   return (
-    <div className="home-container">
+    <div className="home-container" id="inicio">
       <div className="containerFilter">
         <div className="search">
           <input
@@ -51,19 +62,12 @@ export default function Home() {
             className="icon"
           />
         </div>
-        <div style={{ width: 200 }}>
-          {/* options: referenciando os dados, prompt: especificando a mensagem */}
-          <Dropdown
-            options={categories}
-            prompt="Categoria"
-            value={value}
-            onChange={(val) => setValue(val)}
-          />
-        </div>
+        
       </div>
 
       <section className="list-container">
-        {livros?.map((livro) => (
+
+        {currentItems?.map((livro) => (
           <a
             href={`edit-book/${livro.id}`}
             className="book-container"
@@ -72,7 +76,6 @@ export default function Home() {
             <div className="book-infos">
               <p className="book-title">Nome: {livro.nome}</p>
               <p className="book-description">Descrição: {livro.descricao}</p>
-              <p className="book-author">Estoque:{livro.estoque}</p>
               <div className="button">
                 <a href={`details-Book/${livro.id}`}><button>Ver detalhes</button> </a>
               </div>
@@ -81,9 +84,16 @@ export default function Home() {
 
 
           </a>
-
         ))}
       </section>
+      <div className="pagination">
+        {Array.from(Array(pages), (livros, index) => {
+          return <button  value={index} onClick={(e) => setCurrentPage(Number(e.target.value))}>{index + 1}</button>
+        })}
+      </div>
+
     </div>
+
   );
 }
+
